0e2b3f895e579de3862b757e0c64829839d64079
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-19 00:13:30 +0530

    [Mahesh]UC1_Create_a_Stack

---
+++ Stacks_and_Queues/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues
+{
+    internal class Node<T>
+    {
+        public T data; // Adata variable
+        public Node<T> next; // declaring a next node
+        public Node(T data) // Crating constructor having node data as parameter
+        {
+            this.data = data;
+        }
+    }
+}
